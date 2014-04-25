8da6a8b387870ff5a503a9155531e0d7255f3d36
treelite <c.xinle@gmail.com>
2014-04-25 14:05:58 +0800

    update path

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 define(function (require) {
 
     return [
-        {path: '/', action: require('./list/hot'), index: 101},
+        {path: '/index', action: require('./list/hot'), index: 101},
         {path: '/list/new', action: require('./list/new'), index: 102},
         {path: '/detail/:id', action: require('./detail/index'), index: 201}
     ];

be10e6ee17a8b884071cb7171b9a71607a48e76c
treelite <c.xinle@gmail.com>
2015-02-05 14:42:11 +0800

    Del unused files

--- src/config.js
+++
@@ -1,9 +0,0 @@
-define(function (require) {
-
-    return [
-        {path: '/index', action: require('./list/hot'), index: 101, cached: true},
-        {path: '/list/new', action: require('./list/new'), index: 102, cached: true},
-        {path: '/post/:id', action: require('./post/index'), index: 201}
-    ];
-
-});

ab04db0ccc86b7a23d3f8dd168cf136fa1bddcf0
treelite <c.xinle@gmail.com>
2015-03-06 18:00:12 +0800

    Support booting multi page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
  */
 
 var app = require('rebas');
+var path = require('path');
 var ajax = require('saber-ajax').ejson;
 var config = app.get('app');
 
@@ -18,12 +19,23 @@ ajax.rebas(app, ['cookie', 'set-cookie']);
 // 启用存储
 require('saber-storage').rebas(app);
 
-
-app.load(require('./lib/config'));
+var routeInfo = require('./lib/config');
+app.load(routeInfo);
 
 app.setSyncData('root', config.root);
 
+app.after(function (req, res, next) {
+    // 是否启动SPA模式
+    // TODO
+    // 根据访客终端来判断是否启用SPA模式
+    res.templateData.isSPA = false;
+    res.templateData.presenter = routeInfo[res.routeIndex].action;
+    next();
+});
+
 var options = {
+
+    basePath: path.resolve(__dirname, 'lib'),
 
     template: require('./lib/common/common.tpl'),
 
---
+++ src/boot.js
@@ -0,0 +1,32 @@
+/**
+ * @file Boot multi page
+ * @author treelite(c.xinle@gmail.com)
+ */
+
+define(function (require) {
+
+    var app = require('saber-rainbow');
+    var Resolver = require('saber-promise');
+
+    var ajax = require('saber-ajax').ejson;
+    // 设置所有异步请求的URL前缀
+    ajax.config({
+        host: '/api'
+    });
+
+    // Only For Debug
+    // 关闭Promise的异常捕获，方便调试
+    Resolver.disableExceptionCapture();
+
+    // 填充同步的存储数据
+    require('saber-storage').fill(app);
+
+    app.config({
+        isomorphic: true,
+        template: require('./common/common.tpl')
+    });
+
+    return function (config) {
+        app.boot(config);
+    }
+});
